c86ae1689bea22a4821148096f9f31f5a0e22b20
Garnik <nubaryan1@gmail.com>
2026-05-23 18:54:39 +0000

    chore(infra): add local Postgres docker-compose with extensions and db scripts

---
+++ db/init/01-extensions.sql
@@ -0,0 +1,4 @@
+-- Расширения PostgreSQL, нужные AVAStudio.
+-- Выполняется автоматически при первой инициализации контейнера.
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
